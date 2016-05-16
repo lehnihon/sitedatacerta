90947c6ed9ea3f74ef5efe81f5d3792c23936a34
Leandro <lehnihon@hotmail.com>
2016-05-16 13:09:32 -0300

    header datacerta

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="A Data Certa oferece soluções completas de logística para empresas, Cargo, Courier, Armazenagem, Fulfillment, Reversa e Entrega Expressa.">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
