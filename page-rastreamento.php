104aa6518c06a5525d300238b0780aa769c43fa1
Leandro <lehnihon@hotmail.com>
2017-02-10 17:30:46 -0200

    correcao rastreamento

--- page-rastreamento.php
+++ page-rastreamento.php
@@ -12,11 +12,11 @@ get_header(); ?>
 		$valor = $_POST['valor'];
 
 		if($pesq_webserv == 'remessa')
-			$url = "http://54.207.92.38/webserv-main.php?idexterno-webserv=$valor";
+			$url = "http://179.228.162.3:8000/webserv-main.php?idexterno-webserv=$valor";
 		elseif($pesq_webserv == 'cpf')
-			$url = "http://54.207.92.38/webserv-main.php?cpf_webserv=$valor";
+			$url = "http://179.228.162.3:8000/webserv-main.php?cpf_webserv=$valor";
 		else
-			$url = "http://54.207.92.38/webserv-main.php?ra_webserv=$valor";
+			$url = "http://179.228.162.3:8000/webserv-main.php?ra_webserv=$valor";
 		$ch = curl_init();
 		curl_setopt ($ch, CURLOPT_URL, $url);
 		curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 15);
@@ -66,7 +66,7 @@ get_header(); ?>
 				<td><?php echo $contentsArr->status;?></td>
 				<td><?php echo (($contentsArr->codcliente == 6670)?$contentsArr->numconta:$contentsArr->numnotafiscal) ?></td>
 				<td><a href="#" class="ver-info"><img width="25" height="25" src="<?php bloginfo('template_url'); ?>/images/lupa_tabela.png"></a></td>
-				<td><a target="_blank" href="<?php echo "http://54.207.92.38/exibe_pdf_demillus.php?opt=I&idexterno=".$contentsArr->externo; ?>"><img width="25" height="25" src="<?php bloginfo('template_url'); ?>/images/pdf.png"></a></td>
+				<td><a target="_blank" href="<?php echo "http://179.228.162.3:8000/exibe_pdf_demillus.php?opt=I&idexterno=".$contentsArr->externo; ?>"><img width="25" height="25" src="<?php bloginfo('template_url'); ?>/images/pdf.png"></a></td>
 			</tr>
 			<tr class="info-hide" style="display:none">
 				<td colspan="7" style="padding: 50px 10px;">
@@ -185,10 +185,10 @@ get_header(); ?>
 							<div class="movimentacao">
 							  <!-- Mapa -->
 
-							  <iframe src="http://54.207.92.38/movimentacao/mapa.php?lt=<?=$lt?>&ln=<?=$ln?>" id="mapaL" style="float: left; width: 60%; height:500px;"></iframe>
+							  <iframe src="http://179.228.162.3:8000/movimentacao/mapa.php?lt=<?=$lt?>&ln=<?=$ln?>" id="mapaL" style="float: left; width: 60%; height:500px;"></iframe>
 
 							  <!-- Foto -->
-							  <iframe src="http://54.207.92.38/movimentacao/foto.php?id=<?=$idmovimento?>" id="fotoL" style="float: right; width: 40%; height:500px; margin-top:8px"></iframe>
+							  <iframe src="http://179.228.162.3:8000/movimentacao/foto.php?id=<?=$idmovimento?>" id="fotoL" style="float: right; width: 40%; height:500px; margin-top:8px"></iframe>
 
 							</div>		
 						</div>
